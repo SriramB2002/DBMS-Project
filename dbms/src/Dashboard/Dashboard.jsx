b0bb7ec8c734f434844b65313853233763eb6a7e
yashratnani02 <f20200039@hyderabad.bits-pilani.ac.in>
2022-04-14 18:28:29 +0530

    Navbar Updated with Ashwin's Code

--- dbms/src/Dashboard/Dashboard.jsx
+++ dbms/src/Dashboard/Dashboard.jsx
@@ -1,9 +1,24 @@
 import React, { useEffect } from "react";
 import { Redirect } from "react-router-dom";
-function Dashboard(isloggedin) {
-
+import Navbar from "../Components/Navbar";
+function Dashboard() {
+  const navbarItems = [
+    {
+      name:"Home",
+      path:"/login"
+    },
+    {
+      name:"Profile",
+      path:"/login"
+    },
+    {
+      name:"Book",
+      path:"/login"
+    }
+  ];
   return (
     <div>
+      <Navbar navbarItems={navbarItems}/>
       <p>Dashboard</p>
     </div>
   );
